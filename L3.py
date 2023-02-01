aa44417b40979455b8e378921ec99ffaa6e9df76
Katherine O'Roark <123026456+koroark25@users.noreply.github.com>
2023-02-01 15:59:12 -0500

    Add files via upload

---
+++ L3.py
@@ -0,0 +1,8 @@
+# the authors' name are: Katherine O'Roark & Rosemary Hoffman
+import math
+x = math.pi/4
+y = math.pi/3
+num =  math.sqrt((math.tan(x)+1))
+denom = math.sqrt((math.tan(y)+math.sin(y)))
+value = num/denom
+print("value =", value)
---
+++ L4.py
@@ -0,0 +1,8 @@
+# the authors' name are: Katherine O'Roark & Rosemary Hoffman
+import math
+r = 4
+a = 4/math.sqrt(2)
+square= a*a
+circle = math.pi*(r**2)
+area = circle-square
+print("The area between the circle and the square is", area)
